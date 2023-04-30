c84076fed16ebfb4004ca77b38174c422c553e07
Ihor Karen <karenihor@gmail.com>
2023-04-30 11:59:26 +0300

    Container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import React from 'react';
+import { Container } from './Container/Container.styled';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React template
-    </div>
-  );
+  return <Container></Container>;
 };
